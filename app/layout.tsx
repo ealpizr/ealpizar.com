7c2a3c63a0ef9ad36b96bcc3965c767ec90816e6
Enrique Alpízar <ealpizr@gmail.com>
2023-04-09 16:26:42 -0600

    Add custom scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-[#767676] hover:scrollbar-thumb-[#8c8c8c] scrollbar-track-[#e5e5e5] dark:scrollbar-thumb-[#767676] dark:hover:scrollbar-thumb-[#a6a6a6] dark:scrollbar-track-[#424242]">
+        {children}
+      </body>
     </html>
   );
 }
