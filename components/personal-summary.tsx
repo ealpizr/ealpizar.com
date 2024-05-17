6efe45a370a7c01c213f370435a4a57722b539f0
Enrique Alpízar <ealpizr@gmail.com>
2024-05-16 18:09:33 -0600

    Use next/image for fallback

--- components/personal-summary.tsx
+++ components/personal-summary.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import DeveloperAnimation from "~/components/developer-animation";
 import Spotlight from "~/components/spotlight";
 import Socials from "./socials";
@@ -36,7 +37,7 @@ export default function PersonalSummary() {
             className="absolute z-10 w-96 h-96 lg:w-[31.25rem] lg:h-[31.25rem]"
             aria-label="animated illustration of a software developer floating in the air"
           >
-            <img
+            <Image
               id="fallback-developer-svg"
               src="/assets/developer.svg"
               alt="animated illustration of a software developer floating in the air"
